cdb886280fa5bb5b85d02ac4f6c8dddff200a98e
digitalGONG-rgb <bahtjargashi17@gmail.com>
2026-05-31 15:07:55 +0200

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,25 @@
+const jwt = require('jsonwebtoken')
 
+module.exports = (req, res, next) => {
+  const token = req.header('Authorization')
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Access denied'
+    })
+  }
+
+  try {
+    const verified = jwt.verify(
+      token.replace('Bearer ', ''),
+      process.env.JWT_SECRET
+    )
+
+    req.user = verified
+    next()
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Invalid token'
+    })
+  }
+}
